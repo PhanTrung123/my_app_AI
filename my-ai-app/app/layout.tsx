e3f2cc512fb762fac2e1abf67335b0f30f6e243c
PhanTrung123 <phanquangtrung30042004@gmail.com>
2025-07-21 22:41:22 +0700

    #1 start

---
+++ my-ai-app/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin", "vietnamese"],
+  weight: ["400", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "AI-Powered PDF Summarization",
+  description: "Công Cụ tóm tắt PDF sử dụng AI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="vi">
+      <body className={`${roboto.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
